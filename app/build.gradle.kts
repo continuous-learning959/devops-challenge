bacd40201d0d79c492e90cffec7c805f6c741e8a
Leonardo Sato <leo.sato957@gmail.com>
2025-09-09 14:23:14 -0300

    fix(deps): add missing libraries for Compose, Gson, Paging and Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("dagger.hilt.android.plugin")
     id("jacoco")
-    // id("org.owasp.dependencycheck") // commented to avoid errors
 }
 
 android {
@@ -29,34 +30,50 @@ android {
         }
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
     testOptions {
         unitTests.isIncludeAndroidResources = true
     }
+}
+
+dependencies {
+    // Core Android
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+
+    // Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.compose.material:material:1.6.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Paging
+    implementation("androidx.paging:paging-runtime:3.2.1")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.6")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 tasks.withType<JacocoReport> {
     reports {
         xml.required.set(true)
         html.required.set(true)
-    }
-}
-
-// dependencyCheck { // commented to avoid errors
-//     failBuildOnCVSS = 7f
-// }
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.6")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
@@ -77,6 +94,8 @@ tasks.register<JacocoReport>("jacocoTestReport") {
     classDirectories.setFrom(files("${buildDir}/tmp/kotlin-classes/debug"))
 }
 
-kotlin {
-    jvmToolchain(17)
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
