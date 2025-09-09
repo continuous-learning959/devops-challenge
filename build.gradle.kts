a130d92b8dd291682dcbd7bde465aff909baa79b
Leonardo Sato <leo.sato957@gmail.com>
2025-09-09 09:17:52 -0300

    ci: commented owasp plugin to test

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,28 +14,17 @@
  * limitations under the License.
  */
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-        classpath("org.owasp:dependency-check-gradle:8.4.2")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // id("org.owasp.dependencycheck") version "8.4.2" // ⬅️ comentado por enquanto
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        // classpath("org.owasp:dependency-check-gradle:8.4.2") // ⬅️ comentado
     }
 }
 
