e79543f8f8b17c8cc056c8eaee3438d74d620843
Leonardo Sato <leo.sato957@gmail.com>
2025-09-09 14:17:34 -0300

    fix(build): removed redundant plugin line

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.jvm")
     id("jacoco")
     // id("org.owasp.dependencycheck") // commented to avoid errors
 }
